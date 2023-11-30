37a5be03ec6b05b14c86f259cf8448b952152278
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-11-29 23:54:42 -0300

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,8 @@ function styles() {
         .pipe(sass({ outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
+
+exports.default = styles;
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
